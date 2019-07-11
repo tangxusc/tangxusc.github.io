20136d84c23a4201d724c76fac8065f081026965
苏连云 <562050688@qq.com>
2019-07-11 10:51:59 +0800

    修改部署文档

--- deploy.sh
+++ deploy.sh
@@ -10,39 +10,48 @@ echo '6,git add .'
 echo '7,git commit'
 echo '8,git push origin master:master'
 echo '9,git push origin master:master'
+echo '-------------------------------'
 
 sleep 5s
-
+echo '-------------------------------'
 echo '0,git status'
 git checkout origin
 git status
 sleep 2s
-
+echo '-------------------------------'
 echo '1,git add .'
 git add .
 sleep 2s
-
+echo '-------------------------------'
 echo '2,git commit -e'
 git commit -e
 sleep 2s
+echo '-------------------------------'
 echo '3,git checkout master'
 git checkout master
 sleep 2s
+echo '-------------------------------'
 echo '4,git merge origin'
 git merge origin
 sleep 2s
+echo '-------------------------------'
 echo '5,hugo'
 hugo
 sleep 2s
+echo '-------------------------------'
 echo '6,git add .'
 git add .
 sleep 2s
+echo '-------------------------------'
 echo '7,git commit'
 git commit
 sleep 2s
-#echo '8,git push origin master:master'
-#git push origin master:master
-#sleep 2s
-#echo '9,git push origin master:master'
-#9,git push origin master:master
-#sleep 2s
+echo '-------------------------------'
+echo '8,git push origin master:master'
+git push origin master:master
+sleep 2s
+echo '-------------------------------'
+echo '9,git push origin origin:origin'
+9,git push origin origin:origin
+sleep 2s
+echo '部署完成...'
